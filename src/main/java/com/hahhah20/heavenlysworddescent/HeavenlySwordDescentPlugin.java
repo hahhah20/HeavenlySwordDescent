f255959367b06f5591e4b9c4c587df92bf6e17a7
hahhah20 <125357214+hahhah20@users.noreply.github.com>
2026-09-02 00:02:35 +0800

    sync plugin runtime version to 2.1.8

--- src/main/java/com/hahhah20/heavenlysworddescent/HeavenlySwordDescentPlugin.java
+++ src/main/java/com/hahhah20/heavenlysworddescent/HeavenlySwordDescentPlugin.java
@@ -16,7 +16,7 @@ public final class HeavenlySwordDescentPlugin extends JavaPlugin {
         if (getCommand("heavenlysword") != null) {
             getCommand("heavenlysword").setExecutor(new HeavenlySwordCommand(skillManager));
         }
-        getLogger().info("HeavenlySwordDescent V2.1.6 enabled.");
+        getLogger().info("HeavenlySwordDescent V2.1.8 enabled.");
     }
 
     @Override public void onDisable() {
