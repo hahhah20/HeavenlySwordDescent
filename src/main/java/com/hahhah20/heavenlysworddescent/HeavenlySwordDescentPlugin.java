029517b81cb3970d49a35b430ce6401e1408a424
hahhah20 <125357214+hahhah20@users.noreply.github.com>
2026-09-01 19:57:35 +0800

    feat: add main plugin class

---
+++ src/main/java/com/hahhah20/heavenlysworddescent/HeavenlySwordDescentPlugin.java
@@ -0,0 +1,20 @@
+package com.hahhah20.heavenlysworddescent;
+
+import com.hahhah20.heavenlysworddescent.command.HeavenlySwordCommand;
+import com.hahhah20.heavenlysworddescent.manager.CooldownManager;
+import com.hahhah20.heavenlysworddescent.manager.SkillManager;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class HeavenlySwordDescentPlugin extends JavaPlugin {
+    private CooldownManager cooldownManager;
+    private SkillManager skillManager;
+    @Override public void onEnable() {
+        saveDefaultConfig();
+        cooldownManager = new CooldownManager();
+        skillManager = new SkillManager(this);
+        if (getCommand("heavenlysword") != null) getCommand("heavenlysword").setExecutor(new HeavenlySwordCommand(skillManager));
+        getLogger().info("HeavenlySwordDescent V2.1.0 enabled.");
+    }
+    @Override public void onDisable() { if (skillManager != null) skillManager.shutdown(); }
+    public CooldownManager getCooldownManager() { return cooldownManager; }
+}
