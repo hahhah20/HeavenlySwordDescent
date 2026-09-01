9762587a7b033f05ef4a63d228171846f0c2cdda
hahhah20 <125357214+hahhah20@users.noreply.github.com>
2026-09-01 23:02:50 +0800

    set plugin runtime version to 2.1.6

--- src/main/java/com/hahhah20/heavenlysworddescent/HeavenlySwordDescentPlugin.java
+++ src/main/java/com/hahhah20/heavenlysworddescent/HeavenlySwordDescentPlugin.java
@@ -8,13 +8,20 @@ import org.bukkit.plugin.java.JavaPlugin;
 public final class HeavenlySwordDescentPlugin extends JavaPlugin {
     private CooldownManager cooldownManager;
     private SkillManager skillManager;
+
     @Override public void onEnable() {
         saveDefaultConfig();
         cooldownManager = new CooldownManager();
         skillManager = new SkillManager(this);
-        if (getCommand("heavenlysword") != null) getCommand("heavenlysword").setExecutor(new HeavenlySwordCommand(skillManager));
-        getLogger().info("HeavenlySwordDescent V2.1.4 enabled.");
+        if (getCommand("heavenlysword") != null) {
+            getCommand("heavenlysword").setExecutor(new HeavenlySwordCommand(skillManager));
+        }
+        getLogger().info("HeavenlySwordDescent V2.1.6 enabled.");
     }
-    @Override public void onDisable() { if (skillManager != null) skillManager.shutdown(); }
+
+    @Override public void onDisable() {
+        if (skillManager != null) skillManager.shutdown();
+    }
+
     public CooldownManager getCooldownManager() { return cooldownManager; }
 }
