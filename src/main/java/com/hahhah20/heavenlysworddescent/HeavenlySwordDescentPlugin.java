53751458604dc4e082723ee786af439ff2b2ef63
hahhah20 <125357214+hahhah20@users.noreply.github.com>
2026-09-06 17:25:55 +0800

    V2.2.0: initialize split module configuration

--- src/main/java/com/hahhah20/heavenlysworddescent/HeavenlySwordDescentPlugin.java
+++ src/main/java/com/hahhah20/heavenlysworddescent/HeavenlySwordDescentPlugin.java
@@ -1,6 +1,7 @@
 package com.hahhah20.heavenlysworddescent;
 
 import com.hahhah20.heavenlysworddescent.command.HeavenlySwordCommand;
+import com.hahhah20.heavenlysworddescent.config.ModuleConfig;
 import com.hahhah20.heavenlysworddescent.manager.CooldownManager;
 import com.hahhah20.heavenlysworddescent.manager.SkillManager;
 import org.bukkit.plugin.java.JavaPlugin;
@@ -8,20 +9,27 @@ import org.bukkit.plugin.java.JavaPlugin;
 public final class HeavenlySwordDescentPlugin extends JavaPlugin {
     private CooldownManager cooldownManager;
     private SkillManager skillManager;
+    private ModuleConfig moduleConfig;
 
-    @Override public void onEnable() {
+    @Override
+    public void onEnable() {
         saveDefaultConfig();
+        moduleConfig = new ModuleConfig(this);
         cooldownManager = new CooldownManager();
         skillManager = new SkillManager(this);
+
         if (getCommand("heavenlysword") != null) {
             getCommand("heavenlysword").setExecutor(new HeavenlySwordCommand(skillManager));
         }
+
         getLogger().info("HeavenlySwordDescent V2.2.0 enabled.");
     }
 
-    @Override public void onDisable() {
+    @Override
+    public void onDisable() {
         if (skillManager != null) skillManager.shutdown();
     }
 
     public CooldownManager getCooldownManager() { return cooldownManager; }
+    public ModuleConfig getModuleConfig() { return moduleConfig; }
 }
